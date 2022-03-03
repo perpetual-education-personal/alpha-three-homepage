e46b6421eb70af4b57dc1763911da1e5e5705310
SMcGregor199 <shaynemcgregor1@gmail.com>
2022-03-03 15:16:33 -0500

    add visual flourish to shayne card

--- index.php
+++ index.php
@@ -43,6 +43,7 @@
 
       <div class="student shayne">
         <h2 class="attention-voice">Shayne McGregor</h2>
+        <div class="space"></div>
         <p class="intro-voice">I am a Ph.D. candidate in African American Studies as well as a web designer. I believe websites are the most powerful mediums of communication oout today, and we have a responsility to make them create them in ways that will leave an indelible positive impact on the world</p>
         <a href="shayne" target="shayne">Check out my work here</a>
       </div>
