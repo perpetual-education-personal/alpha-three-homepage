e48875b5a8bc3c7e654f2d2223dd0eae0f689f24
Andrew <87679262+andrewbonacci@users.noreply.github.com>
2022-04-02 18:04:10 -0500

    Initial Upload of New Alphanet

---
+++ students.php
@@ -0,0 +1,70 @@
+<?php
+
+$students = [
+
+		[
+		"name" => "Andrew",
+		"description" => "Aspiring web developer who loves working with computers and technology. Let's see what we can do together.",
+		"link" => "Take a look at my portfolio.",
+		"picture" => "images/andrew.png",
+		"site" => "https://peprojects.dev/alpha-3/andrew/",
+		"color" => "darkGray",
+	],
+
+		[
+		"name" => "Jose",
+		"description" => "The sketchiest dude on the web.",
+		"link" => "ENTER",
+		"picture" => "images/jose.png",
+		"site" => "https://peprojects.dev/alpha-3/jose/",
+		"color" => "darkGray",
+	],
+
+	[
+		"name" => "Josh",
+		"description" => "Josh's Stuff",
+		"link" => "Click here",
+		"picture" => "images/josh.png",
+		"site" => "https://peprojects.dev/alpha-3/josh/",
+		"color" => "darkGray",
+	],
+
+	[
+		"name" => "I am sheriffderek",
+		"description" => "See my web page by clicking this hyper-rad hyperlink...",
+		"link" => "I single-cat dare yah",
+		"picture" => "images/derek.png",
+		"site" => "https://peprojects.dev/alpha-3/sheriff/",
+		"color" => "darkGray",
+	],
+
+    [
+		"name" => "TaraUhhhhhh",
+		"description" => "An aspiring web developer and programming enthusiast. On this journey to a new career, I'm collecting experience and inspiration. Come join me and see what I've got so far!",
+		"link" => "Check it out!",
+		"site" => "https://peprojects.dev/alpha-3/tara/",
+		"picture" => "images/tara.png",
+		"color" => "darkGray",
+	],
+
+	[
+		"name" => "Shayne McGregor",
+		"description" => "I am a Ph.D. candidate in African American Studies as well as a web designer. I believe websites are the most powerful mediums of communication out today, and we have a responsility to make them create them in ways that will leave an indelible positive impact on the world",
+		"link" => "Check out my work here",
+		"picture" => "images/shayne.png",
+		"site" => "https://peprojects.dev/alpha-3/shayne/",
+		"color" => "darkGray",
+	],
+
+	[
+		"name" => "Brian",
+		"description" => "Check out my projects and case studies",
+		"link" => "Read more",
+		"picture" => "images/brian.png",
+		"site" => "https://peprojects.dev/alpha-3/bdlowery/",
+		"color" => "darkGray",
+    ],
+
+];
+
+?>
